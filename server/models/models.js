5fea570ff8d96da37fd3102b4a2e75d0063fd164
Malcolm Kam <malcolm.h.kam@gmail.com>
2024-04-02 11:55:50 -1000

    attempt to fix one of the models

--- server/models/models.js
+++ server/models/models.js
@@ -1,12 +1,31 @@
 const connection = require('../db/db');
 
+//for reference
+// CREATE TABLE IF NOT EXISTS terms (
+//   id SERIAL PRIMARY KEY,
+//   terms VARCHAR(255) UNIQUE,
+//   definitions TEXT,
+//   learning INT DEFAULT 0
+// );
 
+// CREATE TABLE IF NOT EXISTS incorrect (
+//   id SERIAL PRIMARY KEY,
+//   definitions TEXT,
+//   term_id INT REFERENCES terms(id)
+// );
+
+// CREATE TABLE IF NOT EXISTS highscores (
+//   id SERIAL PRIMARY KEY,
+//   difficulty VARCHAR(255) UNIQUE,
+//   score INT DEFAULT 0
+// );
 
 exports.getTermsAndIncorrect = (sort, limit) => new Promise((resolve, reject) => {
   let query = `
-    SELECT terms.terms, incorrect.definitions
+    SELECT terms.terms, ARRAY_AGG(incorrect.definitions) AS incorrect_definitions
     FROM terms
     LEFT JOIN incorrect ON terms.id = incorrect.term_id
+    GROUP BY terms.terms
     ORDER BY ${sort};
   `;
   let params;
